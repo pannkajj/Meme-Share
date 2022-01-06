bf695aebaf10c9929b686f9b15358b444f312cca
Pankaj Choudhary <pankajc2041!gmail.com>
2022-01-06 13:21:16 -0600

    v2.1

--- app/src/main/java/me/pankajchoudhary/memeshare/MainActivity.java
+++ app/src/main/java/me/pankajchoudhary/memeshare/MainActivity.java
@@ -1,5 +1,6 @@
 package me.pankajchoudhary.memeshare;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,6 +9,12 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Spinner spinner;
 
+    private DatabaseReference mRootRef;
 
 
     @Override
@@ -22,26 +30,49 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        spinner = findViewById(R.id.spinner);
-        addItemsOnSpinner();
+        mRootRef = FirebaseDatabase.getInstance().getReference().child("memes");
 
-    }
-
-    private void addItemsOnSpinner() {
         List<String> list = new ArrayList<>();
-        list.add("CanadaMeme");
-        list.add("memes");
-        list.add("PrequelMemes");
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, list);
-        spinner.setAdapter(dataAdapter);
+        spinner = findViewById(R.id.spinner);
+
+        mRootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    String name = dataSnapshot.getKey();
+                    list.add(name);
+                }
+
+                ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(getApplicationContext(), R.layout.support_simple_spinner_dropdown_item, list);
+                spinner.setAdapter(dataAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
 
     }
 
     public void startApp(View view) {
-        String source = String.valueOf(spinner.getSelectedItem());
-        Intent i = new Intent(getApplicationContext(),MemesPage.class);
-        i.putExtra("source", source);
-        startActivity(i);
+        String name = String.valueOf(spinner.getSelectedItem());
+        mRootRef.child(name).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String source = snapshot.getValue().toString();
+                Intent i = new Intent(getApplicationContext(), MemesPage.class);
+                i.putExtra("source", source);
+                startActivity(i);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
 
     }
 }
